f70826172ac7cab5d2bdf997af39d18c22458f8e
Kara Isreal <isreal.kara92@gmail.com>
2018-09-05 16:38:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function prependKitten(array, element){
 }
 
 function removeLastKitten(array){
-return kittens.slice(0, kittens.length -1);  
+return kittens.slice(0, kittens.length -1);
 }
 
 function removeFirstKitten(array){
