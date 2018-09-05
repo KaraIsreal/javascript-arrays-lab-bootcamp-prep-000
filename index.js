85b9bf9816896b158ee1350f5ae1c76b9e5b94e7
Kara Isreal <isreal.kara92@gmail.com>
2018-09-05 16:04:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your functions and code here
+function kittens(array){
+  var kittens = ["Milo", "Otis", "Garfield"];
+  return kittens
+}
+
+function destructivelyAppendKitten(name, element){
+kittens.push(Ralph)
+return kittens
+}
+
+function destructivelyPrependKitten(name, element){
+  kittens.unshift(element)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name, element){
+return [...kittens, element];
+}
+
+function prependKitten(name, element){
+  return [element, ...kittens]
+}
+
+function removeLastKitten(){
+kittens.slice(0, kittens.length -1);
+  return kittens
+}
+
+function removeFirstKitten(){
+kittens= kittens.slice(1)
+  return kittens
+}
